18c341c57d72bc82b8fbf1db0e3bb528d560dfb6
Dominic Jenkins <dominicjenkins@hotmail.com>
2020-01-06 22:23:44 -0800

    update dynamic state variable

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Dominic', age: 27 },
+      { name: 'Thomas', age: 51 },
+      { name: 'Emma', age: 26 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
       <h1>Hi, I'm a React App!</h1>
       <p>This is really working!</p>
-      <Person />
+      <button>Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+
       </div>
     );
   }
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -4,8 +4,13 @@
 import React from 'react';
 
 
-const Person = () => {
-    return <p>I'm a Person!</p>
-}
+const Person = (props) => {
+    return (
+    <div>
+        <p>I'm {props.name} and I am {props.age} years old!</p>
+        <p>{props.children}</p>
+    </div>
+    )
+};
 
 export default Person
