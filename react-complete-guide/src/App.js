c6425c18c893153fe366aa45d301328808f56a2f
Dominic Jenkins <dominicjenkins@hotmail.com>
2020-01-07 22:58:51 -0800

    change app from class based to functional based component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,41 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'Dominic', age: 27 },
       { name: 'Thomas', age: 51 },
       { name: 'Peri', age: 25 }
     ],
     otherState: 'some other value'
-  }
+  })
 
-  ageOneYearHandler = () => {
+  const [otherState, setOtherState] = useState('some other value');
+
+  console.log(personsState, otherState);
+
+  const ageOneYearHandler = () => {
     // console.log('Was Clicked!')
     // Don't do this: this.state.persons[0].name = 'Dominic';
-    this.setState({persons: [
-      {name: 'Dominic', age: 28 },
-      {name: 'Thomas', age: 52 },
-      {name: 'Peri', age: 26 },
-    ]})
+    // setState is a react method that allows us to update the state
+    // changing state and props 
+    setPersonsState({
+      persons: [
+        { name: 'Dominic', age: 28 },
+        { name: 'Thom', age: 52 },
+        { name: 'Peri', age: 26 },
+      ]
+    })
   }
 
-  render() {
-    return (
-      <div className="App">
+
+  return (
+    <div className="App">
       <h1>Hi, I'm a React App!</h1>
       <p>This is really working!</p>
-      <button onClick={this.ageOneYearHandler}>Switch Name</button>
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      <button onClick={ageOneYearHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
 
-      </div>
-    );
-  }
+    </div>
+  );
+
 }
 
 // App is exported here and imported into the index.js file which renders to the ReactDOM
-export default App;
+export default app;
+
