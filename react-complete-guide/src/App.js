64c3b289957e55e765b817bb4a030550f10c13f2
Dominic Jenkins <dominicjenkins@hotmail.com>
2020-01-08 23:52:14 -0800

    pass methods through person components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,50 +1,60 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'Dominic', age: 27 },
       { name: 'Thomas', age: 51 },
       { name: 'Peri', age: 25 }
     ],
     otherState: 'some other value'
-  })
+  }
 
-  const [otherState, setOtherState] = useState('some other value');
-
-  console.log(personsState, otherState);
-
-  const ageOneYearHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was Clicked!')
     // Don't do this: this.state.persons[0].name = 'Dominic';
-    // setState is a react method that allows us to update the state
-    // changing state and props 
-    setPersonsState({
+    this.setState({
       persons: [
-        { name: 'Dominic', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Thom', age: 52 },
         { name: 'Peri', age: 26 },
       ]
     })
   }
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Jenkins', age: 28 },
+        { name: event.target.value, age: 52 },
+        { name: 'Roberts', age: 26 },
+      ]
+    })
+  }
 
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App!</h1>
-      <p>This is really working!</p>
-      <button onClick={ageOneYearHandler}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
-
-    </div>
-  );
-
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App!</h1>
+        <p>This is really working!</p>
+        <button onClick={() => this.switchNameHandler('Dominic!!')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Dominic!')}
+          changed={this.nameChangedHandler} />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
 }
 
-// App is exported here and imported into the index.js file which renders to the ReactDOM
-export default app;
-
+export default App;
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,16 +1,15 @@
 // A component is just a function that returns JSX
-// ES6 best practice for using react
-
 import React from 'react';
 
 
-const Person = (props) => {
+const person = (props) => {
     return (
     <div>
-        <p>I'm {props.name} and I am {props.age} years old!</p>
+        <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
         <p>{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name} />
     </div>
     )
 };
 
-export default Person
+export default person;
