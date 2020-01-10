b1f66370bd616f938285e92b1256b15c67f6ee01
Dominic Jenkins <dominicjenkins@hotmail.com>
2020-01-10 08:59:24 -0800

    add simple css style for person cards

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,10 +1,10 @@
 // A component is just a function that returns JSX
 import React from 'react';
-
+import './Person.css';
 
 const person = (props) => {
     return (
-    <div>
+    <div className="Person">
         <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name} />
