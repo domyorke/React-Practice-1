14fe1e89c7c820ba4f08fca4e0b9730e497af9a7
Dominic Jenkins <dominicjenkins@hotmail.com>
2020-01-10 09:14:13 -0800

    css cards, two way binding, inline style practice

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -35,11 +35,21 @@ class App extends Component {
   }
 
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
         <p>This is really working!</p>
-        <button onClick={() => this.switchNameHandler('Dominic!!')}>Switch Name</button>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler('Dominic!!')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age} />
